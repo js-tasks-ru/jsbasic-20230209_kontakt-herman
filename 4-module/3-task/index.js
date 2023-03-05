f516224e9d56df9402c9db1ddb3ea05886000663
Piatrouski Herman <herman.piatrouski@generali.com>
2023-03-05 10:02:39 +0100

    4-MODULE solution tasks

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.rows;
+
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows;
+    const userAge = row[i].cells[1].textContent;
+    const userGender = row[i].cells[2].textContent;
+    const userStatus = row[i].cells[3].getAttribute('data-available');
+
+    if (userAge <= 18) row[i].style.textDecoration = 'line-through';
+    
+    if (userGender === 'm') {
+      row[i].classList.add('male');
+    } else if (userGender === 'f') {
+      row[i].classList.add('female');
+    }
+
+    if (userStatus === 'true') {
+      row[i].classList.add('available');
+    } else if (userStatus === 'false') {
+      row[i].classList.add('unavailable');
+    } else {
+      row[i].setAttribute('hidden', true);
+    }
+  }
 }
