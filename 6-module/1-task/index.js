ddd46c4aeb37330c9353da77ada54e6823fe02cf
Piatrouski Herman <herman.piatrouski@generali.com>
2023-03-23 22:21:30 +0100

    module-6...

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,63 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem;
+    this.render();
   }
+
+  render() {
+    const table = document.createElement('table');
+    const thead = this.createHeder();
+    const tbody = this.createBody();
+    table.append(thead);
+    table.append(tbody);
+    this.elem = table;
+  }
+
+  createHeder() {
+    let createHead = document.createElement('thead');
+    let tr = document.createElement('tr');
+    const headers = ['Имя', 'Возраст', 'Зарплата', 'Город', ''];
+    
+    for (let header of headers) {
+      tr.innerHTML += `<th>${header}</th>`;
+    }
+    createHead.append(tr);
+    return createHead;
+  }
+
+  createBody() {
+    let createBody = document.createElement('tbody');
+
+    for (let item of this.rows) {
+      let tableRow = document.createElement('tr');
+      let lengthItems = Object.keys(item).length;
+      let obj = Object.values(item)
+
+      for (let i = 0; i < lengthItems; i++) {
+        tableRow.innerHTML += `
+          <td>${obj[i]}</td>
+        `
+      }
+
+      let td = document.createElement('td');
+      let remoweBtn = document.createElement('button');
+      remoweBtn.textContent = 'x';
+      
+      remoweBtn.addEventListener('click', ()=> {
+        tableRow.remove();
+      })
+
+      td.append(remoweBtn);
+      tableRow.appendChild(td);
+
+
+      createBody.append(tableRow);
+    }
+
+    return createBody
+  }
+
+
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,97 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+
   constructor(slides) {
     this.slides = slides;
+    this.currentIndex = 0;
+    this.render();
+    this.addEventListeners();
   }
+
+  render() {
+    this.elem = createElement(`
+      <div class="carousel">
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__inner"></div>
+      </div>
+    `);
+
+    let slides = this.slides.map(item => createElement(`
+      <div class="carousel__slide" data-id="${item.id}">
+        <img src="/assets/images/carousel/${item.image}" 
+        class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${item.price.toFixed(2)}</span>
+          <div class="carousel__title">${item.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+    `));
+
+    this.sub('inner').append(...slides);
+
+    this.update();
+  }
+
+  addEventListeners() {
+    this.elem.onclick = ({target}) => {
+      let button = target.closest('.carousel__button');
+      if(button) {
+        let id = target.closest('[data-id]').dataset.id;
+
+        this.elem.dispatchEvent(new CustomEvent('product-add', {
+          detail: id,
+          bubbles: true
+        }));
+      }
+
+      if(target.closest('.carousel__arrow_right')) {
+        this.next();
+      }
+
+      if(target.closest('.carousel__arrow_left')) {
+        this.prev();
+      }
+    };
+  }
+
+  sub(ref) {
+    return this.elem.querySelector(`.carousel__${ref}`);
+  }
+
+  next() {
+    this.currentIndex++;
+    this.update();
+  }
+
+  prev() {
+    this.currentIndex--;
+    this.update();
+  }
+
+  update() {
+    let offset = -this.elem.offsetWidth * this.currentIndex;
+    this.sub('inner').style.transform = `translateX(${offset}px)`;
+
+    if(this.currentIndex == this.slides.length - 1) {
+      this.sub('arrow_right').style.display = 'none';
+    }else {
+      this.sub('arrow_right').style.display = '';
+    }
+
+    if(this.currentIndex == 0) {
+      this.sub('arrow_left').style.display = 'none';
+    }else{
+      this.sub('arrow_left').style.display = '';
+    }
+  }
+
 }
